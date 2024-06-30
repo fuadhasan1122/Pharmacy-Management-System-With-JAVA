7438df76c8ecb8bd2372953f697b8d9a03136ada
fuad hasan <fuad43002@gmail.com>
2024-06-30 10:19:29 +0600

    new update delete

--- Pharmacy Management System/src/UpdateUser.java
+++ Pharmacy Management System/src/UpdateUser.java
@@ -25,11 +25,6 @@ public class UpdateUser extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }
 
-    
-    
-    //new update 3 june 224
-    
-    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
