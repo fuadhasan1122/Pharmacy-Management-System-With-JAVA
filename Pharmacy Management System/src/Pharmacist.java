8a95424b3718c6b2d49bc3f6fc4cfcbb8d79834f
fuad hasan <fuad43002@gmail.com>
2024-07-10 11:28:15 +0600

    update 10 july,2024

--- Pharmacy Management System/src/Pharmacist.java
+++ Pharmacy Management System/src/Pharmacist.java
@@ -59,6 +59,11 @@ public class Pharmacist extends javax.swing.JFrame {
         jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/addMedicine.png"))); // NOI18N
         jButton1.setText("Add Medicine");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 104, 430, -1));
 
         jButton2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
@@ -178,6 +183,11 @@ public class Pharmacist extends javax.swing.JFrame {
            System.exit(0);
         }
     }//GEN-LAST:event_jButton8ActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        new AddMedicine().setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
